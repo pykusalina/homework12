155f039981cef7992ea31fa01291c965f73bdf47
Alina Pykus <alinapykus17@email.com>
2022-09-15 02:16:51 +0300

    add some changes

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-// let xhttp = new XMLHttpRequest();
 
-// xhttp.onreadystatechange = function() {
-//     if(this.readyState == 4 && this.status == 200) {
-//        myFunction(this.responseText) 
-
-       
-//     }
-// }
-// xhttp.open("GET", "", true);
-// xhttp.send();
-
-// function myFunction(data) {
-// console.log(data);
-// }
 
 const vote = document.querySelector('#vote');
 const books = document.querySelector('#books');
